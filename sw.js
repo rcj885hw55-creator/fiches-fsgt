6b2f060d031e219b7929ac9c124ead3cfd43abe1
rcj885hw55-creator <rcj885hw55@privaterelay.appleid.com>
2026-08-02 09:23:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'fsgt-v1';
+const FILES = [
+  './index.html',
+  './fiche_suivi_PE12.html',
+  './fiche_suivi_PE20_N1.html',
+  './fiche_suivi_PE40.html',
+  './fiche_suivi_OWPA20.html',
+  './fiche_suivi_PA40.html',
+  './fiche_suivi_PA60.html',
+  './fiche_suivi_GP.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
